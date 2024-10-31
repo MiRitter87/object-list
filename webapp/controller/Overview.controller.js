cc500ff79613445aaeba540dd1409577081c8c0e
MiRitter87 <michaelritter@gmx.net>
2024-10-31 08:58:19 +0100

    Render column name as Link and add some dummy units.

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -13,6 +13,19 @@ sap.ui.define([
 			
 			oModel.loadData("resources/objects.json");
 			this.getView().setModel(oModel, "objects");
+		},
+		
+		
+		/**
+		 * Handles the link pressed event of the name link.
+		 */
+		onNameLinkPressed : function(oEvent) {
+			var oLinkParent = oEvent.getSource().getParent();
+			var oContext = oLinkParent.getBindingContext("objects");
+			var oObjectData = oContext.getObject();
+			var iObjectId = oObjectData.id;
+			
+			//TODO: Open details view using iObjectId
 		}
 	});
 });
